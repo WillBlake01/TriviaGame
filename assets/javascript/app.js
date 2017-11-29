5f743d6e9c76f1193e7ded14bec64592efec2fa2
William Blake <willblakebooking@gmail.com>
2017-11-29 16:08:11 -0600

    getting close

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,6 @@
+
+$(document).ready(function(){
+
 var radioButton = $(".radioButton");
 var correctAnswers = 0;
 var incorrectAnswers = 0;
@@ -5,33 +8,52 @@ var unanswered = 0;
 
 var answers = [];
 
+$("#quiz").hide();
+
 //on start
 $("#start").on("click", function() {
-//counter
-var count=90;
+$("#start").hide();
+$("#quiz").show().toggleClass("quizContainer");
 
-var counter=setInterval(timer, 1000);
+	//counter
+	var count=90;
 
-function timer() {
-	count=count-1;
-	if (count <= 0)
-		{
-			clearInterval(counter);
+	var counter=setInterval(timer, 1000);
 
-			return;
-		}
-document.getElementById("timer").innerHTML=count + " seconds";
+	function timer() {
+		count=count-1;
+		if (count <= 0)
+			{
+				clearInterval(counter);
+
+				return;
+			}
+	document.getElementById("timer").innerHTML=count + " seconds";
 }
 
 //on submit
 });
 
 $("#submit").on("click", function() {
+
+	$("#quiz").hide().toggleClass("quizContainer");
+
+	
+	
+	$("#results").html("<h3>All Done!</h3>");
+	$("#results").append("<h3>Correct Answers: " + correctAnswers + "<h3>");
+	$("#results").append("<h3>Incorrect Answers: " + incorrectAnswers + "<h3>");
+	$("#results").append("<h3>Unanswered: " + unanswered + "<h3>");
+
+
 	if (answers === radioButton.values()) {
 	correctAnswers++;
 	console.log(correctAnswers);
-} else {
+	} else {
 	incorrectAnswers++;
 	console.log(incorrectAnswers);
 }
+
+});
+
 });
