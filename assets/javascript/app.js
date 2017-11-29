ae321c4452e5f1d36c853f514fa4e1af1d95ba94
William Blake <willblakebooking@gmail.com>
2017-11-29 12:23:21 -0600

    some functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,11 @@
+var radioButton = $(".radioButton");
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
 
+var answers = [];
 
+//on start
 $("#start").on("click", function() {
 //counter
 var count=90;
@@ -17,4 +23,15 @@ function timer() {
 document.getElementById("timer").innerHTML=count + " seconds";
 }
 
+//on submit
+});
+
+$("#submit").on("click", function() {
+	if (answers === radioButton.values()) {
+	correctAnswers++;
+	console.log(correctAnswers);
+} else {
+	incorrectAnswers++;
+	console.log(incorrectAnswers);
+}
 });
