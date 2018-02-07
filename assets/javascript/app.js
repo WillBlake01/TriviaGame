816341ea079a3e75dbf1dace64e8f7bd32d9e062
William Blake <willblakebooking@gmail.com>
2018-02-07 16:10:02 -0600

    got it perfect

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,61 +1,186 @@
 // On page load
 $(document).ready(function () {
 
-  var correctAnswers = '';
-  var incorrectAnswers = '';
-  var unanswered = '';
+  var panel = $('#quiz');
 
-  var answers = [];
+  // Question set
+  var questions = [{
+    question: "How many clubs competed in the Premier League's first season, in 1992-93?",
+    answers: ["18", "22", "24"],
+    correctAnswer: "22"
+  }, {
+    question: "Who has played in a Merseyside derby, a North London derby, a Manchester Derby, a Tyne-Wear derby and a West London derby in the Premier League?",
+    answers: ["Michael Owen", "Emmanuel Adebayor", "Louis Saha"],
+    correctAnswer: "Louis Saha"
+  }, {
+    question: "Which club has scored the most goals from headers in the Premier League?",
+    answers: ["Arsenal", "Stoke City", "Chelsea"],
+    correctAnswer: "Arsenal"
+  }, {
+    question: "Nicolas Anelka is one of two players to have represented Liverpool, Chelsea, and Arsenal in the Premier League. Who is the other?",
+    answers: ["Kolo Toure", "William Gallas", "Yossi Benayoun"],
+    correctAnswer: "Yossi Benayoun"
+  }, {
+    question: "Which Premier League side plays home games at Old Trafford Stadium?",
+    answers: ["Liverpool", "Manchester United", "Newcastle United"],
+    correctAnswer: "Manchester United"
+  }, {
+    question: "Who has scored the most Premier League goals for one club?",
+    answers: ["Thierry Henry at Arsenal", "Wayne Rooney at Manchester United", "Frank Lampard at Chelsea"],
+    correctAnswer: "Wayne Rooney at Manchester United"
+  }, {
+    question: "Which team has conceded the most goals in the Premier League (1,231 goals in 25 seasons)?",
+    answers: ["Tottenham Hotspur", "Aston Villa", "Newcastle United"],
+    correctAnswer: "Tottenham Hotspur"
+  }, {
+    question: "Which team have managed to finish third in the league despite having a negative goal difference (-4)?",
+    answers: ["Norwich City", "Blackburn Rovers", "Arsenal"],
+    correctAnswer: "Norwich City"
+  }, {
+    question: "Which team is the only to manage an unbeaten record in the Premier League as of 2018?",
+    answers: ["Everton", "Blackburn Rovers", "Arsenal"],
+    correctAnswer: "Arsenal"
+  }, {
+    question: "Which team is the best team in the Premier League?",
+    answers: ["Arsenal", "Chelsea", "Manchester United"],
+    correctAnswer: "Arsenal"
+  }];
 
-  $('#quiz').hide();
+// Variable that will hold the setInterval
+var timer;
 
-  // Start button functionality
-  $('#start').on('click', function () {
-    $('#start').hide();
-    $('#quiz').show();
+var game = {
+  correct: 0,
+  incorrect: 0,
+  counter: 120,
+
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
+    }
+  },
+
+  start: function() {
     $('.container').toggleClass('quizContainer');
 
-    // Start counter
-    var count = 90;
+    timer = setInterval(game.countdown, 1000);
 
-    var counter = setInterval(timer, 1000);
+    $("#timer").prepend("<h3>Time Remaining: <span id='counter-number'>120</span> Seconds</h3>");
 
-    function timer() {
-      count = count - 1;
-      if (count <= 0) {
-        clearInterval(counter);
+    $("#start").remove();
 
-        return;
+    for (var i = 0; i < questions.length; i++) {
+      panel.append("<h4>" + questions[i].question + "</h4>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append("<input type='radio' name='question-" + i +
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
       }
-
-      document.getElementById('timer').innerHTML = count + ' seconds';
-
-      // End counter
-      // End start button functionality
     }
-  });
 
-  // Submit button functionality
-  $('#submit').on('click', function () {
+    panel.append("<br><br><button id='done'>Done</button>");
+  },
 
-    $('#quiz').hide();
+  done: function() {
     $('.container').toggleClass('quizContainer');
 
-    $('#results').html('<h3>All Done!!!</h3>');
-    $('#results').append('<p>Correct Answers: ' + correctAnswers + '</p>');
-    $('#results').append('<p>Incorrect Answers: ' + incorrectAnswers + '</p>');
-    $('#results').append('<p>Unanswered: ' + unanswered + '</p>');
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
 
-    // End submit button functionality
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
 
-    if (answers === radioButton.values()) {
-      correctAnswers++;
-      console.log(correctAnswers);
-    } else {
-      incorrectAnswers++;
-      console.log(incorrectAnswers);
-    }
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
 
-  });
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-4']:checked"), function() {
+      if ($(this).val() === questions[4].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-5']:checked"), function() {
+      if ($(this).val() === questions[5].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-6']:checked"), function() {
+      if ($(this).val() === questions[6].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-7']:checked"), function() {
+      if ($(this).val() === questions[7].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+
+  },
+
+  result: function() {
+
+    clearInterval(timer);
+
+    panel.html("<h2>All Done!</h2>");
+    panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
+};
+
+// CLICK EVENTS
+$(document).on("click", "#start", function() {
+  game.start();
+});
+
+
+$(document).on("click", "#done", function() {
+  game.done();
+});
 
 });
